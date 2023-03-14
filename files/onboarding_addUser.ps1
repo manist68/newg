cb33c657e00bf7fea0eb7675fe2c76f3af1acd9a
manist68 <120625434+manist68@users.noreply.github.com>
2023-03-14 17:05:23 +0530

    Update onboarding_addUser.ps1

--- files/onboarding_addUser.ps1
+++ files/onboarding_addUser.ps1
@@ -216,10 +216,13 @@ foreach($line in [System.IO.File]::ReadLines($CustomerConfigURL) | Where {$_ -no
         $ErrorActionPreference = 'SilentlyContinue'
         mongoimport --host $Dbhost --port $Port --db $Dbname --collection $collection --file $finalpath
 
-	# Get-Content -Path .\master\app_project.json
-        $raw = Get-Content -Path C:\agent\_work\4\s\files\master\app_project.json 
-	Write-Host "The vale of app_project.json $raw"
+
 
     	
     #Remove-Item $finalpath
 }
+
+
+	# Get-Content -Path .\master\app_project.json
+        $raw = Get-Content -Path C:\agent\_work\4\s\files\master\app_project.json 
+	Write-Host "The vale of app_project.json $raw"
